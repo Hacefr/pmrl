0aa6af59d9c81125f4d2c55a8d923d4342fa5b55
Hacesgd <162230713+Hacefr@users.noreply.github.com>
2026-06-24 17:43:13 -0400

    Create map.js

---
+++ map.js
@@ -0,0 +1,23 @@
+const TILE_SIZE = 32; // Each grid tile is 32x32 pixels
+const GRID_SIZE = 18; // 18x18 grid matches 576x576 canvas
+
+// 1 = Wall, 0 = Dot to collect
+const gameMap = [,
+ ,
+ ,
+ ,
+ ,
+ ,
+ ,
+ ,
+ ,
+ ,
+ ,
+ ,
+ ,
+ ,
+ ,
+ ,
+ ,
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+];
